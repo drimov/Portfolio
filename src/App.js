be885f00a924a4eafbf3dfd878a0b3725442e763
Dimitri Lhopital <dimitrilho.gmail.com>
2022-01-12 10:45:49 +0100

    :fire:  clean files and add commitizen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
-// import './sass/mystyles.scss';
-import "./css/mystyles.css";
+import './sass/mystyles.scss';
+// import "./css/mystyles.css";
 import { Section } from "react-bulma-components";
 
 import React from "react";
@@ -8,27 +8,8 @@ import HomePage from "./components/HomePage";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import FooterPage from "./components/FooterPage";
-// import FormsPage from './components/FormsPage';
 
 function App() {
-  //   return (
-  //     <div className="App">
-  //       <header className="App-header">
-  //         <img src={logo} className="App-logo" alt="logo" />
-  //         <p>
-  //           Edit <code>src/App.js</code> and save to reload.
-  //         </p>
-  //         <a
-  //           className="App-link"
-  //           href="https://reactjs.org"
-  //           target="_blank"
-  //           rel="noopener noreferrer"
-  //         >
-  //           Learn React
-  //         </a>
-  //       </header>
-  //     </div>
-  //   );
   return (
     <Section id="portfolio">
       <HomePage />
--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-import { Section,Container,Footer,Content,Columns,Navbar,Media,Tile,Card,Image,Box} from "react-bulma-components";
-export { Section,Container,Footer,Content,Columns,Navbar,Media,Tile,Card,Image,Box};
-
